afd17c927a3830764ce7e1dd27720111923cc437
Ahad-Kamal <akamal64@att.net>
2025-09-08 08:13:35 -0600

    Added App.hpp/cpp
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Starship/": change = 424622]

---
+++ Code/Game/App.cpp
@@ -0,0 +1,48 @@
+#include "Starship/Code/Game/App.hpp"
+#include "App.hpp"
+
+App::App()
+{
+
+}
+
+App::~App()
+{
+
+}
+
+void App::RunFrame()
+{
+
+}
+
+void App::Update(float deltaSeconds)
+{
+
+}
+
+void App::Render() const
+{
+
+}
+
+void App::SetIsQuitting()
+{
+
+}
+
+bool App::IsQuitting() const
+{
+
+}
+
+void App::OnKeyDown(unsigned char keyCode)
+{
+
+}
+
+void App::OnKeyUp(unsigned char keyCode)
+{
+
+}
+
