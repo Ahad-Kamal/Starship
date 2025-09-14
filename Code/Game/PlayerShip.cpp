95f73ab74ee521de8678c004d4fd141536d99cd4
Ahad-Kamal <akamal64@att.net>
2025-09-13 20:56:05 -0600

    Updated PlayerShip Part 1
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Starship/": change = 425484]

--- Code/Game/PlayerShip.cpp
+++ Code/Game/PlayerShip.cpp
@@ -2,38 +2,39 @@
 #include "Engine/Core/Vertex.hpp"
 #include "Engine/Core/Engine.hpp"
 #include "Engine/Renderer/Renderer.hpp"
+#include "Game/Entity.hpp"
+#include "Game/GameCommon.hpp"
 
 PlayerShip::PlayerShip(Game* owner, Vec2 const& startingPosition)
+	: Entity( owner, startingPosition )
 {
-	m_position = startingPosition;
+	m_physicsRadius = PLAYER_SHIP_PHYSICS_RADIUS;
+	m_cosmeticRadius = PLAYER_SHIP_COSMETIC_RADIUS;
+	InitializeLocalVerts();
 }
 
 PlayerShip::PlayerShip(Game* owner, Vec2 const& startingPosition, Vec2 const& startingVelocity)
+	: Entity( owner, startingPosition )
 {
-	m_position = startingPosition;
 	m_velocity = startingVelocity;
 }
 
 void PlayerShip::Update(float deltaSeconds)
 {
+	UpdateFromKeyboard( deltaSeconds );
+	BounceOffWalls();
+	
 	m_position += m_velocity * deltaSeconds;
-
-	/*
-	if (g_app->wasJustPressed(' '))
-	{
-		FireBullet();
-	}
-
-	if (g_app->isKeyDown('A')
-	{
-		orientationDegrees += playerTrnSpeedDegreesPerSecond * deltaSeconds
-	}
-	*/
-
 }
 
 void PlayerShip::Render() const
 {
+	/*if( m_isDead )
+	{
+
+	}*/
+
+	// Assignment 1 PlayerShip
 	Vertex shipVerts[3];
 
 	shipVerts[0].m_pos = Vec3( (m_position.x ) + 4.f, m_position.y, 0.f);
@@ -49,4 +50,62 @@ void PlayerShip::Render() const
 	shipVerts[2].m_uvTexCoords = Vec2( 0.f, 0.f );
 	
 	g_engine->m_render->DrawVertexArray( 3, shipVerts );
+}
+
+void PlayerShip::InitializeLocalVerts()
+{
+	// Nose Cone
+	m_localVerts[0].m_pos = Vec3( 1.f, 0.f, 0.f );
+	m_localVerts[1].m_pos = Vec3( 0.f, 1.f, 0.f );
+	m_localVerts[2].m_pos = Vec3( 0.f, -1.f, 0.f );
+
+	// Left Wing
+	m_localVerts[3].m_pos = Vec3( 2.f, 1.f, 0.f );
+	m_localVerts[4].m_pos = Vec3( 0.f, 2.f, 0.f );
+	m_localVerts[5].m_pos = Vec3( -2.f, 1.f, 0.f );
+
+	// Right Wing
+	m_localVerts[6].m_pos = Vec3( 2.f, -1.f, 0.f );
+	m_localVerts[7].m_pos = Vec3( -2.f, -1.f, 0.f );
+	m_localVerts[8].m_pos = Vec3( 0.f, -2.f, 0.f );
+
+	// Body (Quad Tri 1)
+	m_localVerts[9].m_pos = Vec3( 0.f, 1.f, 0.f );
+	m_localVerts[10].m_pos = Vec3( -2.f, -1.f, 0.f );
+	m_localVerts[11].m_pos = Vec3( 0.f, -1.f, 0.f );
+
+	// Body (Quad Tri 2)
+	m_localVerts[12].m_pos = Vec3( 0.f, 1.f, 0.f );
+	m_localVerts[13].m_pos = Vec3( -2.f, 1.f, 0.f );
+	m_localVerts[14].m_pos = Vec3( -2.f, -1.f, 0.f );
+
+	for( int vertIndex = 0; vertIndex < NUM_SHIP_VERTS; vertIndex++ )
+	{
+		m_localVerts[vertIndex].m_color = Rgba8( 102, 153, 204 );
+	}
+}
+
+void PlayerShip::UpdateFromKeyboard( float deltaSeconds )
+{
+	/*
+	if (g_app->wasJustPressed(' '))
+	{
+	FireBullet();
+	}
+
+	if (g_app->isKeyDown('A')
+	{
+	orientationDegrees += playerTrnSpeedDegreesPerSecond * deltaSeconds
+	}
+	*/
+}
+
+void PlayerShip::BounceOffWalls()
+{
+
+}
+
+void PlayerShip::Respawn()
+{
+
 }
--- Code/Game/PlayerShip.hpp
+++ Code/Game/PlayerShip.hpp
@@ -1,20 +1,32 @@
 #pragma once
 #include "Engine/Math/Vec2.hpp"
+#include "Game/Entity.hpp"
+#include "Engine/Core/Vertex.hpp"
 
 class Game;
+constexpr int NUM_SHIP_TRIS = 5;
+constexpr int NUM_SHIP_VERTS = 3 * NUM_SHIP_TRIS;
 
 //-----------------------------------------------------------------------------------------------
-class PlayerShip
+class PlayerShip : public Entity
 {
 public:
 	PlayerShip( Game* owner, Vec2 const& startingPosition );
 	PlayerShip( Game* owner, Vec2 const& startingPosition, Vec2 const& startingVelocity );
 	~PlayerShip() = default;
-	void Update( float deltaSeconds );
-	void Render() const;
 
-public:
-	Vec2	m_position;
-	Vec2	m_velocity;
-	float	m_health;
+	virtual void Update( float deltaSeconds ) override;
+	virtual void Render() const override;
+
+private:
+	void InitializeLocalVerts();
+	void UpdateFromKeyboard( float deltaSeconds );
+	void BounceOffWalls();
+	void Respawn();
+
+private:
+	Vertex	m_localVerts[NUM_SHIP_VERTS];
+	bool m_isTurningLeft = false;
+	bool m_isTurningRight = false;
+	bool m_isThrusting = false;
 };
