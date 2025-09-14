66174db0edb3f0506d16f0ecee1f3c9b5a7dbe3a
Ahad-Kamal <akamal64@att.net>
2025-09-13 18:27:43 -0600

    Added Bullet functions
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Starship/": change = 425472]

---
+++ Code/Game/Bullet.cpp
@@ -0,0 +1,65 @@
+#include "Bullet.hpp"
+#include "Entity.hpp"
+#include "Engine/Core/Vertex.hpp"
+#include "Engine/Math/MathUtils.hpp"
+#include "Engine/Core/Engine.hpp"
+#include "Engine/Renderer/Renderer.hpp"
+#include "Game/GameCommon.hpp"
+
+
+Bullet::Bullet( Game* owner, Vec2 const& startPos )
+	: Entity( owner, startPos )
+{
+	m_physicsRadius = BULLET_PHYSICS_RADIUS;
+	m_cosmeticRadius = BULLET_COSMETIC_RADIUS;
+	InitializeLocalVerts();
+}
+
+Bullet::~Bullet()
+{
+
+}
+
+void Bullet::Update( float deltaSeconds )
+{
+	m_position += (m_velocity * deltaSeconds );
+
+	if( isOffScreen() )
+	{
+		m_isDead = true;
+		m_isGarbage = true;
+	}
+}
+
+void Bullet::Render() const
+{
+	Vertex tempWorldVerts[NUM_BULLET_VERTS];
+
+	for( int vertIndex = 0; vertIndex < NUM_BULLET_VERTS; vertIndex++ )
+	{
+		tempWorldVerts[vertIndex] = m_localVerts[vertIndex];
+		TransformPositionXY3D(tempWorldVerts[vertIndex].m_pos, 1.f, m_orientationDegrees, m_position );
+	}
+
+	g_engine->m_render->DrawVertexArray( NUM_BULLET_VERTS, tempWorldVerts );
+}
+
+void Bullet::InitializeLocalVerts()
+{
+	// Bullet
+	m_localVerts[0].m_pos = Vec3( 0.5f, 0.f, 0.f );
+	m_localVerts[1].m_pos = Vec3( 0.f, 0.5f, 0.f );
+	m_localVerts[2].m_pos = Vec3( 0.f, -0.5f, 0.f );
+	m_localVerts[0].m_color = Rgba8( 255, 255, 255, 255 );
+	m_localVerts[1].m_color = Rgba8( 255, 255, 0, 255 );
+	m_localVerts[2].m_color = Rgba8( 255, 255, 0, 255 );
+
+	// Trail
+	m_localVerts[3].m_pos = Vec3( 0.f, -0.5f, 0.f );
+	m_localVerts[4].m_pos = Vec3( 0.f, 0.5f, 0.f );
+	m_localVerts[5].m_pos = Vec3( -2.f, 0.f, 0.f );
+	m_localVerts[3].m_color = Rgba8( 255, 0, 0, 255 );
+	m_localVerts[4].m_color = Rgba8( 255, 0, 0, 255 );
+	m_localVerts[5].m_color = Rgba8( 255, 0, 0, 0 );
+}
+
