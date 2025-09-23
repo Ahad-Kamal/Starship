69854b8de3a0ef0ee2074ec94859e75c362c9feb
Ahad-Kamal <akamal64@att.net>
2025-09-23 17:26:46 -0600

    Implemented Debris functions
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Starship/": change = 427408]

--- Code/Game/Debris.cpp
+++ Code/Game/Debris.cpp
@@ -1,9 +1,19 @@
+#include "Engine/Core/Vertex.hpp"
+#include "Engine/Math/MathUtils.hpp"
+#include "Game/Entity.hpp"
+#include "Game/Debris.hpp"
+#include "Engine/Core/Engine.hpp"
+#include "Engine/Renderer/Renderer.hpp"
 
+Debris::Debris( Game* owner, Vec2 const& startingPos, Vec2 const& vel, float radius, Rgba8 color, float lifetimeSeconds )
+	: Entity( owner, startingPos )
+{
 
-/*
+}
+
 void Debris::Update( float deltaSeconds )
 {
-	Entity::Update(deltaSeconds)
+	m_ageSeconds += deltaSeconds;
 
 	if( m_ageSeconds >= m_lifeTimeSeconds)
 	{
@@ -11,17 +21,16 @@ void Debris::Update( float deltaSeconds )
 	}
 }
 
-void Debris::Render const
+void Debris::Render() const
 {
-	Vertex tempDebrisVerts[DEBRIS_NUM_VERTS]
-	for( int = 0; i < DEBRIS_NUM_VERTS; i++ )
+	Vertex tempDebrisVerts[ NUM_DEBRIS_VERTS ];
+	for( int i = 0; i < NUM_DEBRIS_VERTS; i++ )
 	{
 		tempDebrisVerts[i] = m_localVerts[i];
-		float alphaFloat = RangeMapClamped( m_ageSeconds, 0.f, m_lifespanSeconds, 127.f, 0.f );
-		tempDebrisVerts[i].m_color.a = static_cast<unsigned_char>( alphaFloat );
+		float alphaFloat = RangeMapClamped( m_ageSeconds, 0.f, m_lifeTimeSeconds, 127.f, 0.f );
+		tempDebrisVerts[i].m_color.a = static_cast<unsigned char>( alphaFloat );
 	}
 
-	TransformVertexArrayXY3D( DEBRIS_NUM_Verts, tempDebrisVerts, 1.f, m_orientationDegrees, m_position );
-	g_engine->m_render->DrawVertexArray( DEBRIS_NUM_VERTS, tempDebrisVerts ); 
+	TransformVertexArrayXY3D( NUM_DEBRIS_VERTS, tempDebrisVerts, 1.f, m_orientationDegrees, m_position );
+	g_engine->m_render->DrawVertexArray( NUM_DEBRIS_VERTS, tempDebrisVerts ); 
 }
-*/
---
+++ Code/Game/Debris.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "Engine/Core/Vertex.hpp"
+#include "Game/Entity.hpp"
+
+class Game;
+class Vec2;
+class Rgba8;
+
+constexpr int NUM_DEBRIS_SIDES = 16;
+constexpr int NUM_DEBRIS_TRIS = NUM_DEBRIS_SIDES;
+constexpr int NUM_DEBRIS_VERTS = 3 * NUM_DEBRIS_TRIS;
+
+class Debris : public Entity
+{
+public:
+	Debris( Game* owner, Vec2 const& startingPos, Vec2 const& vel, float radius, Rgba8 color, float lifetimeSeconds );
+	~Debris() = default;
+
+	virtual void Update( float deltaSeconds ) override;
+	virtual void Render() const override; 
+
+private:
+	Vertex m_localVerts[ NUM_DEBRIS_VERTS ];
+	float m_lifeTimeSeconds = 2.f;
+};
