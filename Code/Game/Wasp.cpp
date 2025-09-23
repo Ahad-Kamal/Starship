d974e2cd151d3e542b9fc5dd584f21a49d371a2b
Ahad-Kamal <akamal64@att.net>
2025-09-22 20:36:16 -0600

    Implemented Wasp functions
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Starship/": change = 427276]

---
+++ Code/Game/Wasp.cpp
@@ -0,0 +1,93 @@
+#include "Engine/Math/MathUtils.hpp"
+#include "Engine/Core/Engine.hpp"
+#include "Engine/Renderer/Renderer.hpp"
+#include "Game/Wasp.hpp"
+#include "Game/GameCommon.hpp"
+#include "Game/PlayerShip.hpp"
+
+Wasp::Wasp( Game* owner, Vec2 const& startingPosition )
+	: Entity( owner, startingPosition )
+{
+	m_health = 2;
+	m_physicsRadius = WASP_PHYSICS_RADIUS;
+	m_cosmeticRadius = WASP_COSMETIC_RADIUS;
+	InitializeLocalVerts();
+}
+
+void Wasp::Update( float deltaSeconds )
+{
+	m_velocity += GetForwardNormal() * WASP_ACCELERATION * deltaSeconds;
+	m_position += ( m_velocity * deltaSeconds );
+}
+
+void Wasp::Update( float deltaSeconds, PlayerShip const& ship )
+{
+	m_velocity += GetForwardNormal() * WASP_ACCELERATION * deltaSeconds;
+
+	if( ship.IsAlive() )
+	{
+		m_orientationDegrees = Atan2Degrees( ship.m_position.y - m_position.y, ship.m_position.x - m_position.x );
+	}
+}
+
+void Wasp::Render() const
+{
+	Vertex tempWorldVerts[ NUM_WASP_VERTS ];
+
+	for( int vertIndex = 0; vertIndex < NUM_WASP_VERTS; vertIndex++ )
+	{
+		tempWorldVerts[ vertIndex ] = m_localVerts[ vertIndex ];
+	}
+
+	TransformVertexArrayXY3D( NUM_WASP_VERTS, tempWorldVerts, 1.f, m_orientationDegrees, m_position );
+	g_engine->m_render->DrawVertexArray( NUM_WASP_VERTS, tempWorldVerts );
+}
+
+void Wasp::InitializeLocalVerts()
+{
+	// Head
+	m_localVerts[ 0 ].m_pos = Vec3( 2.f, 0.f, 0.f );
+	m_localVerts[ 1 ].m_pos = Vec3( 1.f, 0.5f, 0.f );
+	m_localVerts[ 2 ].m_pos = Vec3( 1.f, -0.5f, 0.f );
+
+	// Body (Quad Tri 1)
+	m_localVerts[ 3 ].m_pos = Vec3( -1.f, 0.5f, 0.f );
+	m_localVerts[ 4 ].m_pos = Vec3( 1.f, 0.5f, 0.f );
+	m_localVerts[ 5 ].m_pos = Vec3( 1.f, -0.5f, 0.f );
+
+	// Body (Quad Tri 2)
+	m_localVerts[ 6 ].m_pos = Vec3( -1.f, 0.5f, 0.f );
+	m_localVerts[ 7 ].m_pos = Vec3( -1.f, -0.5f, 0.f );
+	m_localVerts[ 8 ].m_pos = Vec3( 1.f, -0.5f, 0.f );
+
+	// Left Big Wing
+	m_localVerts[ 9 ].m_pos = Vec3( 0.f, 0.5f, 0.f );
+	m_localVerts[ 10 ].m_pos = Vec3( 1.f, 0.5f, 0.f );
+	m_localVerts[ 11 ].m_pos = Vec3( 0.5f, 2.f, 0.f );
+
+	// Right Big Wing
+	m_localVerts[ 12 ].m_pos = Vec3( 0.f, -0.5f, 0.f );
+	m_localVerts[ 13 ].m_pos = Vec3( 1.f, -0.5f, 0.f );
+	m_localVerts[ 14 ].m_pos = Vec3( 0.5f, -2.f, 0.f );
+
+	// Left Small Wing
+	m_localVerts[ 15 ].m_pos = Vec3( 0.f, 0.5f, 0.f );
+	m_localVerts[ 16 ].m_pos = Vec3( -1.f, 0.5f, 0.f );
+	m_localVerts[ 17 ].m_pos = Vec3( -0.5f, 1.f, 0.f );
+
+	// Right Small Wing
+	m_localVerts[ 18 ].m_pos = Vec3( 0.f, -0.5f, 0.f );
+	m_localVerts[ 19 ].m_pos = Vec3( -1.f, -0.5f, 0.f );
+	m_localVerts[ 20 ].m_pos = Vec3( -0.5f, -1.f, 0.f );
+
+	// Tail (Quad Tri 2)
+	m_localVerts[ 21 ].m_pos = Vec3( -1.f, 0.5f, 0.f );
+	m_localVerts[ 22 ].m_pos = Vec3( -1.f, -0.5f, 0.f );
+	m_localVerts[ 23 ].m_pos = Vec3( -2.f, 0.f, 0.f );
+
+	for( int vertIndex = 0; vertIndex < NUM_WASP_VERTS; vertIndex++ )
+	{
+		m_localVerts[ vertIndex ].m_color = Rgba8( 255, 255, 0 );
+	}
+}
+
---
+++ Code/Game/Wasp.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "Engine/Math/Vec2.hpp"
+#include "Engine/Core/Vertex.hpp"
+#include "Game/Entity.hpp"
+
+class Game;
+class PlayerShip;
+constexpr int NUM_WASP_TRIS = 8;
+constexpr int NUM_WASP_VERTS = 3 * NUM_WASP_TRIS;
+
+class Wasp : public Entity
+{
+public:
+	Wasp( Game* owner, Vec2 const& startingPosition );
+	~Wasp() = default;
+
+	virtual void Update( float deltaSeconds ) override;
+	void Update( float deltaSeconds, PlayerShip const& ship );
+	virtual void Render() const override;
+
+private:
+	void InitializeLocalVerts();
+
+private:
+	Vertex m_localVerts[ NUM_WASP_VERTS ];
+};
