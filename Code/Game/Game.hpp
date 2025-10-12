12997331168021a2936157d80a25679685913ae6
Ahad-Kamal <akamal64@att.net>
2025-10-12 16:14:51 -0600

    Adjusted speeds to account for bigger world
    [git-p4: depot-paths = "//depot/C35/Students/ahadk/SD/Starship/": change = 431448]

--- Code/Game/Game.hpp
+++ Code/Game/Game.hpp
@@ -92,7 +92,7 @@ private:
 	Vertex m_starVertsFar[ NUM_STAR_VERTS ];
 
 	int m_waveNumber = 0;
-	int m_numAsteroidsPerWave[ NUM_WAVES ] = { 2, 2, 2, 2, 2 };
+	int m_numAsteroidsPerWave[ NUM_WAVES ] = { 12, 12, 12, 12, 12 };
 	int m_numBeetlesPerWave[ NUM_WAVES ] = { 1, 3, 0, 3, 5 };
 	int m_numWaspsPerWave[ NUM_WAVES ] = { 0, 0, 1, 2, 3 };
 	int m_endGameTimer = 0;
